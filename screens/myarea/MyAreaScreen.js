88711a24c1b48c20c94e0d227af4ff233e3b3d5c
dippyhaze <mattia.dipetro@aesystech.it>
2020-03-09 09:42:03 +0100

    Refinement of MyArea screen

--- screens/myarea/MyAreaScreen.js
+++ screens/myarea/MyAreaScreen.js
@@ -30,20 +30,19 @@ const MyAreaScreen = ({navigation, getUserLoggedOut}) => {
         <Text style={styles.userName}> {userInfo.displayName} </Text>
       </View>
 
-      <View
-        style={{
-          borderWidth: 1,
-          borderColor: '#fefefe',
-          height: 420,
-          width: 280,
-          alignSelf: 'center',
-          marginVertical: '10%',
-          borderRadius: 7,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Icon name="camera" size={90} color="#fefefe" />
-      </View>
+      <TouchableOpacity style={styles.photoScanContainer}>
+        <Icon
+          name="camera"
+          size={90}
+          color="#fefefe"
+          style={{
+            shadowColor: '#000',
+            shadowOffset: {width: 3, height: 3},
+            shadowOpacity: 0.8,
+          }}
+        />
+        <Text style={styles.photoScanText}> Scan your Betting Slip </Text>
+      </TouchableOpacity>
 
       <TouchableOpacity onPress={SignOutUser} style={styles.logoutButton}>
         <Text style={styles.logoutButtonText}> LOGOUT </Text>
@@ -74,6 +73,34 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: '#fefefe',
   },
+  userName: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#fefefe',
+    textAlign: 'left',
+  },
+  photoScanContainer: {
+    borderWidth: 1,
+    borderColor: '#fefefe',
+    height: 420,
+    width: 280,
+    alignSelf: 'center',
+    marginVertical: '10%',
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 3, height: 3},
+    shadowOpacity: 0.8,
+  },
+  photoScanText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#fefefe',
+    shadowColor: '#000',
+    shadowOffset: {width: 3, height: 3},
+    shadowOpacity: 0.8,
+  },
   logoutButton: {
     backgroundColor: '#C9082A',
     height: 35,
@@ -85,12 +112,6 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOffset: {width: 3, height: 3},
     shadowOpacity: 0.8,
-  },
-  userName: {
-    fontSize: 20,
-    fontWeight: '600',
-    color: '#fefefe',
-    textAlign: 'left',
   },
   logoutButtonText: {
     fontSize: 17,
